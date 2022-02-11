ab37834a82aaa6ac852b1d3a094a42ba06059b0b
Starky <eduardogpc1@gmail.com>
2022-02-11 14:33:57 -0500

    update

--- index.js
+++ index.js
@@ -6,6 +6,5 @@ const app = express();
 app.use('/', router);
 
 const port = process.env.PORT || 4000;
-const host = 'localhost' || '0.0.0.0';
 
-app.listen(port, host, () => console.log('server ready'));
+app.listen(port, '0.0.0.0', () => console.log('server ready'));
